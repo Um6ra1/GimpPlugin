916ebbf524618dee2a292b2f925377d9b5f5cfe5
Vi3 <um6ra1@gmail.com>
2017-10-28 00:07:26 +0900

    Some refactored

--- GimpPlugin/Dll.h
+++ GimpPlugin/Dll.h
@@ -4,34 +4,34 @@ void	Msg(PCSTR psFmt, ...) {
 #define MSG_BUF_LEN	0x200
 	va_list ap;
 	va_start(ap, psFmt);
-	char sMsg[MSG_BUF_LEN];
-	::vsprintf(sMsg, psFmt, ap);
+	char msg[MSG_BUF_LEN];
+	vsprintf(msg, psFmt, ap);
 	va_end(ap);
-	::MessageBoxA(NULL, sMsg, "DEBUG MSG", MB_OK | MB_ICONEXCLAMATION);
+	MessageBoxA(NULL, msg, "DEBUG MSG", MB_OK | MB_ICONEXCLAMATION);
 }
 
 typedef PVOID (__cdecl *Func)(...);
 
 class Dll {
 	HMODULE	hModule_;
-	bool	bAlreadyLoaded_;
+	bool bAlreadyLoaded_;
 
 public:
 	bool Invalid() { return hModule_ == NULL; }
 	
-	Func	GetFunction(PCSTR psFuncName) {
-		FARPROC pfProc = ::GetProcAddress(hModule_, psFuncName);
+	Func GetFunction(PCSTR psFuncName) {
+		FARPROC pfProc = GetProcAddress(hModule_, psFuncName);
 		
-		if( !pfProc ) ::Msg("Not found such function: %s", psFuncName);
+		if( !pfProc ) Msg("Not found such function: %s", psFuncName);
 		return (Func)pfProc;
 	}
 	Dll(PCSTR psDllName) : bAlreadyLoaded_(false) {
-		if( (hModule_ = ::GetModuleHandle(psDllName)) ) {
-			//::Msg("Already loaded: %s", psDllName);
+		if( (hModule_ = GetModuleHandle(psDllName)) ) {
+			//Msg("Already loaded: %s", psDllName);
 			bAlreadyLoaded_ = true;
 			return;
 		}
-		if( !(hModule_ = ::LoadLibrary(psDllName)) ) ::Msg("Error loading: %s", psDllName);
+		if( !(hModule_ = LoadLibrary(psDllName)) ) Msg("Error loading: %s", psDllName);
 	}
-	~Dll()	{ if(!bAlreadyLoaded_) ::FreeLibrary(hModule_); }
+	~Dll() { if(!bAlreadyLoaded_) FreeLibrary(hModule_); }
 };
--- GimpPlugin/Hello/Main.cpp
+++ GimpPlugin/Hello/Main.cpp
@@ -11,33 +11,33 @@ void	Run(PCSTR psName, int params, const GimpParam *pSrc, int *pReturns, GimpPar
 const GimpPlugInInfo	PLUG_IN_INFO = { NULL, NULL, Query, Run };
 
 int WINAPI	WinMain(HINSTANCE hInst, HINSTANCE, char *psCmdLine, int nCmdShow) {
-	Dll	libFFI("libffi-6.dll");	if(libFFI.Invalid())	return -1;
-	Dll	libGIo("libgio-2.0-0.dll");	if(libGIo.Invalid())	return -1;
-	Dll	libGLib("libglib-2.0-0.dll");	if(libGLib.Invalid())	return -1;
-	Dll	libGMod("libgmodule-2.0-0.dll");	if(libGMod.Invalid())	return -1;
-	Dll	libGObj("libgobject-2.0-0.dll");	if(libGObj.Invalid())	return -1;
-	Dll	libIntl("libintl-8.dll");	if(libIntl.Invalid())	return -1;
+	Dll libFFI("libffi-6.dll"); if(libFFI.Invalid()) return -1;
+	Dll libGIo("libgio-2.0-0.dll"); if(libGIo.Invalid()) return -1;
+	Dll libGLib("libglib-2.0-0.dll"); if(libGLib.Invalid()) return -1;
+	Dll libGMod("libgmodule-2.0-0.dll"); if(libGMod.Invalid()) return -1;
+	Dll libGObj("libgobject-2.0-0.dll"); if(libGObj.Invalid()) return -1;
+	Dll libIntl("libintl-8.dll"); if(libIntl.Invalid()) return -1;
 
-	Dll	libGimpBase("libgimpbase-2.0-0.dll");	if(libGimpBase.Invalid())	return -1;
-	Dll	libGimp("libgimp-2.0-0.dll");	if(libGimp.Invalid())	return -1;
+	Dll libGimpBase("libgimpbase-2.0-0.dll"); if(libGimpBase.Invalid()) return -1;
+	Dll libGimp("libgimp-2.0-0.dll"); if(libGimp.Invalid()) return -1;
 	
-	Func	pfnGimpMain	=  libGimp.GetFunction("gimp_main");
+	Func pfnGimpMain = libGimp.GetFunction("gimp_main");
 	
 	return (int)pfnGimpMain(&PLUG_IN_INFO, __argc, __argv);
 }
 
-PCSTR	PLUGIN_NAME		= "plug-in-Hello";
-PCSTR	PLUGIN_BLURB	= "Hello world";
-PCSTR	PLUGIN_HELP		= "Hello world plugin on Windows.";
-PCSTR	PLUGIN_AUTHOR	= "Um6ra1";
-PCSTR	PLUGIN_COPYRIGHT		= "Copyright (C) Um6ra1";
-PCSTR	PLUGIN_DATE				= "2016";
-PCSTR	PLUGIN_MENULABEL	= "_HelloWorld";
-PCSTR	PLUGIN_IMGTYPES		= "RGB*, GRAY*";
-PCSTR	PLUGIN_MENUPATH		= "<Image>/Filters/VsNative-Plugin";
+PCSTR PLUGIN_NAME = "plug-in-Hello";
+PCSTR PLUGIN_BLURB	= "Hello world";
+PCSTR PLUGIN_HELP = "Hello world plugin on Windows.";
+PCSTR PLUGIN_AUTHOR = "Um6ra1";
+PCSTR PLUGIN_COPYRIGHT = "Copyright (C) Um6ra1";
+PCSTR PLUGIN_DATE = "2016";
+PCSTR PLUGIN_MENULABEL = "_HelloWorld";
+PCSTR PLUGIN_IMGTYPES = "RGB*, GRAY*";
+PCSTR PLUGIN_MENUPATH = "<Image>/Filters/VsNative-Plugin";
 
 void	Query() {
-	Dll	libGimp("libgimp-2.0-0.dll");	if(libGimp.Invalid())	return;
+	Dll libGimp("libgimp-2.0-0.dll"); if(libGimp.Invalid()) return;
 
 	Func pfnGimpInstallProcedure	= libGimp.GetFunction("gimp_install_procedure");
 	Func pfnGimpPluginMenuRegister	= libGimp.GetFunction("gimp_plugin_menu_register");
@@ -59,7 +59,7 @@ void	Query() {
 	);
 
 	int status = (int)pfnGimpPluginMenuRegister(PLUGIN_NAME, PLUGIN_MENUPATH);
-	if(!status)	::Msg("gimp_plugin_menu_register() failed!");
+	if(!status)	Msg("gimp_plugin_menu_register() failed!");
 }
 
 void	ImgProc(Dll &libGimp, GimpDrawable *pDrawable);
@@ -73,5 +73,5 @@ void	Run(PCSTR psName, int srcNum, const GimpParam *pSrc, int *pDstNum, GimpPara
 	values[0].data.d_status	= GIMP_PDB_SUCCESS;
 	
 	int runMode = values[0].data.d_int32;
-	if(runMode != GIMP_RUN_INTERACTIVE)	::Msg("Hello world!");
+	if(runMode != GIMP_RUN_INTERACTIVE) Msg("Hello world!");
 }
